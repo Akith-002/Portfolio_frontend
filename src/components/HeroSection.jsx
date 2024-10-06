7d90a3aaaaf428a6e1161fa2296aa2f52fb799ea
Akith-002 <chandinubaa.22@uom.lk>
2024-10-06 12:19:27 +0530

    added resume button

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -26,25 +26,50 @@ const HeroSection = () => {
         <p className="text-4xl md:text-6xl w-11/12 md:w-3/4 animate-slidein mb-4 md:mb-6 opacity-0 [--slidein-delay:500ms] text-center">
           A Passionate Developer & Undergraduate from the University of Moratuwa
         </p>
-        <a href="#tech">
-          <Button className="flex items-center gap-2 bg-white text-black rounded-full animate-slidein opacity-0 [--slidein-delay:700ms] hover:shadow-[0_0_2px_2px] hover:shadow-yellow-400">
-            Explore
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="h-5 w-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-              />
-            </svg>
-          </Button>
-        </a>
+        <div className="flex gap-4 md:gap-10 items-center justify-center flex-wrap mx-2">
+          <a href="#tech">
+            <Button className="flex items-center gap-2 md:text-md bg-white text-black rounded-full animate-slidein opacity-0 [--slidein-delay:700ms] hover:shadow-[0_0_2px_2px] hover:shadow-yellow-400">
+              Explore
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                />
+              </svg>
+            </Button>
+          </a>
+
+          <a
+            href="https://drive.google.com/file/d/1LtOgFX_-LlAwDL2PdOuw_J1GOH423mT6/view?usp=sharing"
+            target="_blank"
+          >
+            <Button className="flex items-center px-7 gap-2 md:text-md  bg-black text-yellow-500 rounded-full animate-slidein opacity-0 [--slidein-delay:700ms] hover:shadow-[0_0_2px_2px] hover:shadow-yellow-400">
+              Resume
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                />
+              </svg>
+            </Button>
+          </a>
+        </div>
       </div>
     </section>
   );
